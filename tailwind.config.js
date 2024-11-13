10678181b79384396d5230ac636a30ab730c848f
pramod-12345 <pramod.kumar.meeting@gmail.com>
2024-11-13 21:55:01 +0530

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,37 @@ export default {
       },
       colors: {
         label: "#333333",
-        primary: "#039375",
+        primary: "#673CFF",
+        accent: "#141525",
+        accentLight: "#1B1E2A",
         themeBlack: "#0b0d1c",
+        textMenu: "#9298B4",
+        textMenuHover: "#664FFF",
+        gradientBorder: "#5A5F9C"
       },
       height: {
-        "table-header": "44px", // Custom height for the table header
-        "table-row": "78px", // Custom height for the table rows
-        "table-full": "824px", // Custom height for the full table
+        "table-header": "44px", 
+        "table-row": "78px", 
+        "table-full": "824px",
       },
+      width: {
+        '5px': "5px",
+        "9px": "9px",
+        "18px": "18px",
+        "260px": "260px",
+        "352px": "352px",
+        "1120px": "1120px"
+      },
+      maxWidth: {
+        '11rem': '11rem',
+        '32p': '32%'
+      },
+      maxHeight: {
+        '17.5rem': '17.5rem',
+      },
+      spacing: {
+        "72px": "72px", // Add your custom spacing here
+      }
     },
   },
   plugins: [],
