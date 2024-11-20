cc6909f06e776eb66aadcb73106668a914b58a02
pramod-12345 <pramod.kumar.meeting@gmail.com>
2024-11-20 18:39:11 +0530

    modal fixes

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import Typography from './typography';
-import closeIcon from "../../assets/svg/close.svg"
+import React from "react";
+import Typography from "./typography";
+import closeIcon from "../../assets/svg/close.svg";
 
-const Modal = ({ title , onClose, children }) => {
+const Modal = ({ title, onClose, children }) => {
   return (
-    <div className="w-[600px] mx-auto">
-      <div className="bg-blackRussian rounded-[20px] p-8 text-white">
-        <div className="flex justify-between items-center">
-          <Typography color={"white"} content={title} variant={"size20Semibold"}/>
-          <button onClick={onClose}>
-            <img src={closeIcon} alt='Close'/>
-          </button>
-        </div>
+    <div className="fixed inset-0 flex z-20 items-center justify-center bg-black bg-opacity-50">
+      <div className="w-[600px] mx-auto">
+        <div className="bg-blackRussian rounded-[20px] p-8 text-white">
+          <div className="flex justify-between items-center">
+            <Typography
+              color={"white"}
+              content={title}
+              variant={"size20Semibold"}
+            />
+            <button onClick={onClose}>
+              <img src={closeIcon} alt="Close" />
+            </button>
+          </div>
 
-        <div className="">
-          {children}
+          <div className="">{children}</div>
         </div>
       </div>
     </div>
